ac71224562b2b6b2392c83c4dcb4b24968323a57
Iarynovskyi <dimayar20061@gmail.com>
2025-07-19 23:13:41 +0300

    new

---
+++ .branchlintrc.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pattern: '^(feature|bugfix|chore|test)/[0-9]+/[a-z0-9]+(-[a-z0-9]+)*$|(hotfix|release)/[0-9]+\\.[0-9]+\\.[0-9]+$',
+
+  examples: [
+    'feature/1234/add-login-ui',
+    'bugfix/23/fix-login-error',
+    'chore/987/update-dependencies',
+    'test/456/write-login-tests',
+    'hotfix/1.0.1',
+    'release/1.2.3'
+  ],
+
+  exclude: ['main', 'develop']
+};
---
+++ commitlint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // A new feature
+        'fix', // A bug fix
+        'docs', // Documentation changes
+        'style', // Code style changes (formatting, etc)
+        'refactor', // Code changes that neither fix bugs nor add features
+        'perf', // Performance improvements
+        'test', // Adding or fixing tests
+        'build', // Changes to build system or dependencies
+        'ci', // Changes to CI configuration
+        'chore', // Other changes that don't modify src or test files
+        'revert' // Reverts a previous commit
+      ]
+    ]
+  }
+};
