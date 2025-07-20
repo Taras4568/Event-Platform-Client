60b674c1492bc32ecb68b1fa8a52ce5a71f73013
Iarynovskyi <dimayar20061@gmail.com>
2025-07-20 18:58:00 +0300

    feat: oops

--- .branchlintrc.config.js
+++ .branchlintrc.config.js
@@ -1,9 +1,9 @@
 export default {
   pattern:
-    '^(feat|bugfix|chore|test)/[0-9]+/[a-z0-9]+(-[a-z0-9]+)*$|(hotfix|release)/[0-9]+\\.[0-9]+\\.[0-9]+$',
+    '^(feature|bugfix|chore|test)/[0-9]+/[a-z0-9]+(-[a-z0-9]+)*$|(hotfix|release)/[0-9]+\\.[0-9]+\\.[0-9]+$',
 
   examples: [
-    'feat/1234/add-login-ui',
+    'feature/1234/add-login-ui',
     'bugfix/23/fix-login-error',
     'chore/987/update-dependencies',
     'test/456/write-login-tests',
