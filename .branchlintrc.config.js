3420b9cc78c56f411facbb89f6efe94ddbe516e5
Iarynovskyi <dimayar20061@gmail.com>
2025-07-20 18:55:10 +0300

    feat: add test filters block

--- .branchlintrc.config.js
+++ .branchlintrc.config.js
@@ -1,9 +1,8 @@
 export default {
   pattern:
     '^(feature|bugfix|chore|test)/[0-9]+/[a-z0-9]+(-[a-z0-9]+)*$|(hotfix|release)/[0-9]+\\.[0-9]+\\.[0-9]+$',
-
   examples: [
-    'feature/1234/add-login-ui',
+    'feat/1234/add-login-ui',
     'bugfix/23/fix-login-error',
     'chore/987/update-dependencies',
     'test/456/write-login-tests',
